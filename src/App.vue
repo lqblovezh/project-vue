83bc14cf55e466ffd0cdcc97fbd45fe4b2570339
lqblovezh <1277678760@qq.com>
2019-12-09 22:50:28 +0800

    rtmp 文件测试

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,51 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <div>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </div>
-    <div class="div">
-      <el-checkbox
-        :indeterminate="isIndeterminate"
-        v-model="checkAll"
-        @change="handleCheckAllChange"
-      >全选</el-checkbox>
-      <div style="margin: 15px 0;"></div>
-      <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-        <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
-      </el-checkbox-group>
-    </div>
-    <!-- <div>
-      <vueQriousjs :config="config"></vueQriousjs>
-      <div style="width:400px;margin:0 auto;">
-        <el-input v-model="config.value" placeholder="二维码内容"></el-input>
-        <el-slider v-model="config.size" :max="400" :min="200"></el-slider>
-        <el-color-picker v-model="config.background" @change="colorChange"></el-color-picker>
-        <el-color-picker v-model="config.foreground"></el-color-picker>
-      </div>
-    </div>-->
-    <!-- <el-steps :space="200" :active="1" finish-status="finish">
-      <el-step title="已完成"></el-step>
-      <el-step title="进行中"></el-step>
-      <el-step title="步骤 3"></el-step>
-    </el-steps>-->
-    <router-view />
+    <img src="./assets/logo.png" />
+    <!-- <video
+      id="my-player"
+      class="video-js"
+      controls
+      preload="auto"
+      data-setup="{}"
+      width="500"
+      height="400"
+    >
+      <source src="rtmp://isxsports.com/dext/live" type="rtmp/flv" />
+    </video> -->
+    <video
+      id="my-video"
+      class="video-js"
+      controls
+      preload="auto"
+      width="640"
+      height="440"
+      poster="MY_VIDEO_POSTER.jpg"
+      data-setup="{}"
+    >
+      <source src="rtmp://58.200.131.2:1935/livetv/gxtv" type="rtmp/flv" />
+      <!-- <source src="http://vjs.zencdn.net/v/oceans.webm" type="video/webm" />
+      <source src="http://vjs.zencdn.net/v/oceans.ogv" type="video/ogg" /> -->
+    </video>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-    const cityOptions = ['上海', '北京', '广州', '深圳']
-    return {
-      config: {
-        background: '#fff',
-        backgroundAlpha: 0.8,
-        foreground: '#000',
-        foregroundAlpha: 0.8,
-        level: 'Q',
-        padding: 25,
-        size: 250,
-        value: 'https://github.com/slimcheng/vue-qriousjs',
-      },
-      checkAll: false,
-      checkedCities: ['上海', '北京', '广州', '深圳'],
-      cities: cityOptions,
-      isIndeterminate: true,
-    }
+  mounted() {
+    var options = {};
+    var myPlayer = videojs("my-video");
+    videojs("my-video").ready(function() {
+      var myPlayer = this;
+      myPlayer.play();
+    });
   },
-  created() {
-    console.log(this.axios);
-    this.axios.get('/api/setting/position/banner_pc').then(res => {
-      console.log(res, 111111)
-    })
-  },
-  methods: {
-    colorChange(val) {
-      console.log(val)
-    },
-    handleCheckAllChange(val) {
-      this.checkedCities = val ? cityOptions : []
-      this.isIndeterminate = false
-    },
-    handleCheckedCitiesChange(value) {
-      let checkedCount = value.length
-      this.checkAll = checkedCount === this.cities.length
-      this.isIndeterminate =
-        checkedCount > 0 && checkedCount < this.cities.length
-    },
-  },
-}
+  methods: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
